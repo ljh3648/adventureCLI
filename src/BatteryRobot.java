beba30629f810c731025b03d46cf2d46b1a2eb92
Lee Jihun <ljh09060@gmail.com>
2025-09-13 19:47:56 +0900

    chore: 코드 정리

--- src/BatteryRobot.java
+++ src/BatteryRobot.java
@@ -3,35 +3,39 @@ public class BatteryRobot extends Robot {
 
     BatteryRobot(String name) {
         super(name);
-        this.batteryLevel = 100;
+        this.batteryLevel = 50;
     }
 
     public int getBatteryLevel() {
         return batteryLevel;
     }
 
-    public void setBatteryLevel(int batteryLevel) {
-        this.batteryLevel = batteryLevel;
+    public void chargeBatteryLevel(int amount) {
+        if (amount > 0) {
+            this.batteryLevel += amount;
+
+            if (this.batteryLevel > 100) {
+                this.batteryLevel = 100;
+            }
+        }
     }
 
-//    public void chargeBattery() {
-//        batteryLevel = 100;
-//        System.out.printf("[Robot: %s]: 배터리를 충전이 완료되었습니다.\n", name);
-//    }
-
     public boolean useBattery(int amount) {
-        if(batteryLevel - amount < 0) {
+        if (batteryLevel - amount < 0) {
             System.out.printf("[Robot: %s]: 배터리가 부족하여 실행되지 못하고 전원이 종료됩니다.\n", name);
+
+            batteryLevel = 0;
             powerOff();
             return false;
         }
+
         batteryLevel -= amount;
         return true;
     }
 
     @Override
     public void powerOn() {
-        if(batteryLevel > 0) {
+        if (batteryLevel > 0) {
             super.powerOn();
         } else {
             System.out.printf("[Robot: %s]: 충전이 필요합니다.\n", name);
@@ -42,23 +46,4 @@ public class BatteryRobot extends Robot {
     public void showStatus() {
         System.out.printf("[Robot: %s]: Power status: %s Battery level: %d\n", name, powerStatus ? "ON" : "OFF", batteryLevel);
     }
-
-//    @Override
-//    public void showPowerControlMenu() {
-//        super.showPowerControlMenu();
-//        System.out.print("3. 배터리 충전\n");
-//        System.out.print("선택: ");
-//    }
-
-//    @Override
-//    public void powerController(int selectCommand) {
-//        switch (selectCommand) {
-//            case 3:
-//                chargeBattery();
-//                break;
-//            default:
-//                super.powerController(selectCommand);
-//                break;
-//        }
-//    }
 }
