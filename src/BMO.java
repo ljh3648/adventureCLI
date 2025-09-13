9f0ae4f002c9c04be920d7ad42471d92856f4777
Lee Jihun <ljh09060@gmail.com>
2025-09-13 19:49:14 +0900

    chore: 코드 정리

--- src/BMO.java
+++ src/BMO.java
@@ -1,30 +1,35 @@
-public class BMO extends BatteryRobot {
+ public class BMO extends BatteryRobot implements Runnable {
     boolean isQuiet;
 
     BMO() {
         super("BMO");
-    }
-
-
-    public void quietMode() {
-        if(!useBattery(1)){
-            return;
-        }
-        isQuiet = true;
-        try {
-            Thread.sleep(10);
-            System.out.printf("\n[Robot: %s]: 네... 몇초 정도는 조용히 있어볼게요.\n", name);
-            Thread.sleep(1000 * 30);
-        } catch (InterruptedException e) {
-            isQuiet = false;
-            return;
-        }
         isQuiet = false;
     }
 
+    @Override
+    public void run() {
+        isQuiet = true;
+        quietMode();
+    }
+
+    public void quietMode() {
+        try {
+            Thread.sleep(1000 * 30);
+        } catch (InterruptedException e) {
+            System.out.printf("\n[ERROR]: 스레드 예외처리 발생: %s\n", e.getMessage());
+        } finally {
+            if (powerStatus) {
+                System.out.printf("\n[Robot: %s]: 저 이제 응답 하려고 하는데 괜찮죠?\n", name);
+                System.out.print("선택: ");
+            }
+
+            isQuiet = false;
+        }
+    }
+
     public void fakeFarting() {
-        if(useBattery(5)){
-            System.out.printf("[Robot: %s]: poot! \uD83D\uDCA8 \uD83D\uDCA8 \uD83D\uDCA8 \n", name);
+        if (useBattery(10)) {
+            System.out.printf("[Robot: %s]: 부르륵 뿌뿌뿡! \uD83D\uDCA8 \uD83D\uDCA8 \uD83D\uDCA8 \n", this.name);
         }
     }
 
@@ -33,9 +38,9 @@ public class BMO extends BatteryRobot {
     }
 
     @Override
-    public void showCommand(){
+    public void showCommand() {
         super.showCommand();
-        System.out.print("1. 10초간 응답하지 않기\n");
+        System.out.print("1. 30초간 응답하지 않기\n");
         System.out.print("2. 가짜 방귀 소리\n");
         System.out.print("3. 노래 부르기\n");
         System.out.print("선택: ");
@@ -43,13 +48,21 @@ public class BMO extends BatteryRobot {
 
     @Override
     public void action(int selectCommand) {
-        if(powerStatus == false){
-            System.out.printf("[Robot: %s]: 전원을 먼저 켜주면 안될까요?\n", name);
+        if (!powerStatus) {
+            System.out.printf("[System]: %s 전원을 먼저 켜주면 안될까요?\n", name);
+            return;
+        } else if (isQuiet) {
+            System.out.printf("[System]: %s 응답이 없어요....\n", name);
             return;
         }
+
         switch (selectCommand) {
             case 1:
-                quietMode();
+                if (!useBattery(1)) return;
+                // quietMode(); 조용모드 실행
+                System.out.printf("\n[Robot: %s]: 네... 몇초 정도는 조용히 있어볼게요.\n", name);
+                Thread thread = new Thread(this);
+                thread.start();
                 break;
             case 2:
                 fakeFarting();
